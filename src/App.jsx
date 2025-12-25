90df9168a2678e7777a6e3ce60c0b51060c1a8d4
TPODanzig <tpodanzig@gmail.com>
2025-12-25 16:46:16 +0100

    oopsie daisy, was wat code vergeten

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,13 @@ import Footer from "./components/Footer.jsx";
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-(--bg) text-(--text)">
-      {/* Header komt hier */}
-      <header className="bg-(--surface) p-4 border-b border-(--bordercolor)">
-        <p className="text-center">Header placeholder</p>
-      </header>
+      <Header />
 
-      {/* Pagina content via Outlet */}
       <main className="flex-1 container mx-auto">
         <Outlet />
       </main>
 
-      {/* Footer komt hier */}
-      <footer className="bg-(--surface) p-4 border-t border-(--bordercolor)">
-        <p className="text-center text-(--muted)">Footer placeholder</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
