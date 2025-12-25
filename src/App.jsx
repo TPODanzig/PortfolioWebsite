c7f7ec2f3c6ff11096fa76f37b65f5a04cdf88ef
TPODanzig <tpodanzig@gmail.com>
2025-12-25 16:41:18 +0100

    page contents test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+
 export default function App() {
-  //return (
-    
-  //);
+  return (
+    <div className="min-h-screen flex flex-col bg-(--bg) text-(--text)">
+      {/* Header komt hier */}
+      <header className="bg-(--surface) p-4 border-b border-(--bordercolor)">
+        <p className="text-center">Header placeholder</p>
+      </header>
+
+      {/* Pagina content via Outlet */}
+      <main className="flex-1 container mx-auto">
+        <Outlet />
+      </main>
+
+      {/* Footer komt hier */}
+      <footer className="bg-(--surface) p-4 border-t border-(--bordercolor)">
+        <p className="text-center text-(--muted)">Footer placeholder</p>
+      </footer>
+    </div>
+  );
 }
