6c1f57d3d59fa345640d31b241d9ea219814febb
TPODanzig <tpodanzig@gmail.com>
2025-12-26 22:40:14 +0100

    Prefab project details geimplementeerd

---
+++ src/siteConfig.js
@@ -0,0 +1,32 @@
+export const siteConfig = {
+  name: "Robin van Veluw",
+  role: "XR Developer",
+  tagline: "Hallo, ik ben Robin, wat vind je van mijn projecten?",
+  aboutImage: "https://static0.polygonimages.com/wordpress/wp-content/uploads/2024/11/Hei-1.jpg?w=1600&h=1200&fit=crop",
+  cv: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+
+  socials: {
+    email: "r.m.vanveluw@gmail.com",
+    github: "https://github.com/TPODanzig",
+    linkedin: "https://linkedin.com/",
+    itch: "https://itch.io/dashboard",
+  },
+
+  aboutLong: `
+    Hallo,
+    Ik ben Robin, van de XR klas van 2024-2029!
+
+    Ik doe deze studie omdat ik producten wil creëren waar ik zelf lol van krijg, dit voltooi ik door eigen projecten te starten en mijn best te doen een uniek eindproduct te krijgen voor mijn school opdrachten.
+
+    Mijn interesse voor development komt al uit mijn jeugd, waar ik uren lang zou kijken naar videos over hoe bepaalde games werken, en hoe ze in elkaar werden gezet, dit zorgde ervoor dat ik graag games wou maken, wat uiteindelijk veranderde in een interesse voor coderen.
+    Ook heb ik een paar jaar dungeons and dragons gespeeld, waar ik naar een tijdje spel leider zou worden, dat er ook voor heeft gezord dat ik een gevoel kreeg voor ervaringen maken voor anderen.
+
+    Maar aan het einde van de dag heb ik lol in wat ik doe, en ben ik trots op wat ik neerzet, wat extreem geëleveerd word doordat ik dit doe voor school.
+    Fijne dag naar U, geachte lezer,
+    Tot ziens!
+  `,
+
+  softSkills: ["Samenwerken met artists", "Unieke oplossingen voor problemen vinden", "Groeps afspraken realistisch maken en nakomen"],
+
+  hardSkills: ["Unity & XR Interaction Toolkit", "C# Scripting", "Git & Version Control", "slight proficiency in python"],
+};
