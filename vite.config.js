2d37080ecc14d98414ed0d4062c48be1b67dce64
TPODanzig <tpodanzig@gmail.com>
2026-03-04 10:36:03 +0100

    help

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   base: '/PortfolioWebsite/',
+    build: {
+    outDir: 'docs'
+  }
 })
